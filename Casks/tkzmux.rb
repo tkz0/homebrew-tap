e6211c2100d81c64ba5917c21bcafe1aab788dde
tkzmux release <tkzmux-release@users.noreply.github.com>
2026-09-21 05:14:28 +0000

    tkzmux 1.2.3

--- Casks/tkzmux.rb
+++ Casks/tkzmux.rb
@@ -1,6 +1,6 @@
 cask "tkzmux" do
-  version "1.2.2"
-  sha256 "85809af36c82e183cd1618e1d393c6b123d65cfa238a8874b5569db1ceb56975"
+  version "1.2.3"
+  sha256 "61ce949d6e292a523157bfdb9175a7690af8e78cfa61f75831a86ed26cef233b"
 
   url "https://github.com/tkz0/tkzmux/releases/download/v#{version}/tkzmux-#{version}-arm64.zip"
   name "tkzmux"
